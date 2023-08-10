2c835d99a20d3fb87052de35aa4a6541c5494159
aa-r-ju <aarju3447@gmail.com>
2023-08-10 21:33:27 +0545

    Feat:trying something new

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let myVariable = 55:
   return (
     <div className="App">
       <header className="App-header">
+        <di> {myVariable}</di>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
