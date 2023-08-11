873cbed47c704cfd9fbfc755bc88ee59fb72f7e0
aa-r-ju <aarju3447@gmail.com>
2023-08-11 20:54:10 +0545

    feat:doing experiment with react

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
-  <>
+<React.Fragment>
 <h1> Hello World!!</h1>
 <p>  How are you??? </p>
 <p>  I am<b> Aarju Pariyar</b> </p>
-</>,
+<p> i am doing <b>Great</b></p>
+</React.Fragment>,
 document.getElementById("root"));
