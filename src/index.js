5c3b34e0134f5c044537337075eed714988f61c9
aa-r-ju <aarju3447@gmail.com>
2023-08-11 23:44:30 +0545

    feat:added image in jsx

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import  React from "react"
+import React from "react"
 import  ReactDOM from "react-dom"
 
-const name = "Aarju"
-const currentDate = new Date().toLocaleDateString();
-const currentTime = new Date().toLocaleTimeString();
 
-ReactDOM.render( 
+const name = "Aarju"
+const img1= "https://fastly.picsum.photos/id/458/200/300.jpg?hmac=2d4QALnxYzzYD8DSwuNsa7lutkdOxLsKojuxbNfd31I"
+const img2 = "https://fastly.picsum.photos/id/487/200/200.jpg?hmac=acsZoF1o2OcaX5f6QJScaXgr19-izrth6ECMjr7hG9M"
+ReactDOM.render(
   <>
-    <h1>Hello, My name is {name} </h1>
-    <p> Todays Date is {currentDate}</p>
-    <p>  Current Time is {currentTime}  </p>
-  </>,
-  document.getElementById("root")
+    <h1 contentEditable = "true"> Hello, my name is {name}</h1>
+    <img src = {img1} alt = "randomImage"  />
+    <img src = {img2} alt = "randomImage1"  />
+  </>,document.getElementById("root")
 )
