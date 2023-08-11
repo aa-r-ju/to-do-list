20b3e4dc741ca211453bab7c459a5e71f1c9f2b9
aa-r-ju <aarju3447@gmail.com>
2023-08-11 21:44:43 +0545

    experiment with react

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import  ReactDOM  from "react-dom"
 
 const name = "Aarju"
 const lname = "Pariyar"
+const age =  22
 
 ReactDOM.render(
 <>
-<h1> Hello my name is {name} {lname}</h1>
+<h1> Introduction </h1>
+<p> Hello my name is {name} {lname}</p>
+<p> I am {age} years old</p>
+<p> my birth date is {20 + 5} August</p>
 </>, 
 document.getElementById("root"))
