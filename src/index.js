0defe73fe4fc236649335a2e6e6221699cb9c43e
aa-r-ju <aarju3447@gmail.com>
2023-08-11 16:18:33 +0545

    Feat:trying new from react

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-let React = require("react");
-let ReactDOM = require("react-dom");
+// let React = require("react");
+// let ReactDOM = require("react-dom");
+import React from "react";
+import ReactDOM from "react-dom";
 
 ReactDOM.render(<h1> Hello World!! How are you??? </h1>,document.getElementById("root"));
