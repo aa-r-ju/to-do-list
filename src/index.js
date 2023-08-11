5ae87a605069ad4d78d0ec68e54dc756028f7dc1
aa-r-ju <aarju3447@gmail.com>
2023-08-11 20:36:40 +0545

    feat:trying something new

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1> Hello World!! How are you??? </h1>,document.getElementById("root"));
+ReactDOM.render(
+  <>
+<h1> Hello World!!</h1>
+<p>  How are you??? </p>
+<p>  I am<b> Aarju Pariyar</b> </p>
+</>,
+document.getElementById("root"));
