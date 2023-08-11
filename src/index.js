d9154ba6b094c8677d61a82c421b36b080ca9b1e
aa-r-ju <aarju3447@gmail.com>
2023-08-11 22:13:49 +0545

    trying react

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const age =  22
 ReactDOM.render(
 <>
 <h1> Introduction </h1>
-<p> Hello my name is {name} {lname}</p>
+<p> Hello my name is {name + " " + lname}</p>
 <p> I am {age} years old</p>
 <p> my birth date is {20 + 5} August</p>
 </>, 
