15e59fc2f62e65eb0d0e1833b92d703855d399b8
aa-r-ju <aarju3447@gmail.com>
2023-08-12 20:29:00 +0545

    feat:adding image using react

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import  ReactDOM from "react-dom"
 const name = "Aarju"
 const img1= "https://fastly.picsum.photos/id/458/200/300.jpg?hmac=2d4QALnxYzzYD8DSwuNsa7lutkdOxLsKojuxbNfd31I"
 const img2 = "https://fastly.picsum.photos/id/487/200/200.jpg?hmac=acsZoF1o2OcaX5f6QJScaXgr19-izrth6ECMjr7hG9M"
+const img3 = "https://fastly.picsum.photos/id/487/200/200.jpg?hmac=acsZoF1o2OcaX5f6QJScaXgr19-izrth6ECMjr7hG9M"
+const img4 = "https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI"
 ReactDOM.render(
   <>
     <h1 contentEditable = "true"> Hello, my name is {name}</h1>
     <img src = {img1} alt = "randomImage"  />
     <img src = {img2} alt = "randomImage1"  />
+    <img src = {img3} alt = "randomImage1"  />
+    <img src = {img4} alt = "randomImage1"  />
   </>,document.getElementById("root")
 )
