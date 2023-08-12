d0ce05b988ba245a0d91dc62f6d7654bd1393479
aa-r-ju <aarju3447@gmail.com>
2023-08-12 20:34:52 +0545

    adding links on react

--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@ const img1= "https://fastly.picsum.photos/id/458/200/300.jpg?hmac=2d4QALnxYzzYD8
 const img2 = "https://fastly.picsum.photos/id/487/200/200.jpg?hmac=acsZoF1o2OcaX5f6QJScaXgr19-izrth6ECMjr7hG9M"
 const img3 = "https://fastly.picsum.photos/id/487/200/200.jpg?hmac=acsZoF1o2OcaX5f6QJScaXgr19-izrth6ECMjr7hG9M"
 const img4 = "https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI"
+const links = "https://www.aarjupariyar.com"
 ReactDOM.render(
   <>
     <h1 contentEditable = "true"> Hello, my name is {name}</h1>
     <img src = {img1} alt = "randomImage"  />
     <img src = {img2} alt = "randomImage1"  />
     <img src = {img3} alt = "randomImage1"  />
-    <img src = {img4} alt = "randomImage1"  />
+    <a href = {links} target = "aarju">   
+     <img src = {img4} alt = "randomImage1"  />
+ </a>
   </>,document.getElementById("root")
 )
