10442c9bb9bc669ab6915c4ed790c97a412a4ea3
aa-r-ju <aarju3447@gmail.com>
2023-08-13 16:45:56 +0545

    Para.jsx

--- src/index.js
+++ src/index.js
@@ -1,46 +1,18 @@
-// import React from "react"
-// import ReactDOM from "react-dom"
-
-// let crnTime = new Date();
-// crnTime = crnTime.getHours();
-// let greeting = " ";
-// let make = { };
-
-// if(crnTime >= 1 && crnTime < 12) {
-//   greeting = "Good Morning";
-//   make.color = "orange"
-// } else if (crnTime >= 12 && crnTime < 19) {
-//   greeting = "Good Afternoon";
-//   make.color = "Green"
-// } else {
-//   greeting = "Good Night";
-//   make.color = "maroon"
-
-// }
-// ReactDOM.render(<h1>Hello Sir, <span cssSt ={make}> {greeting} </span> </h1>,
-// document.getElementById("root"))
-
-
-
 import React from "react"
-import ReactDOM from "react-dom"
-
-let curDate = new Date(2023,8,25,15);
-curDate = curDate.getHours();
-let greeting = " ";
-let cssStyle = { };
-
-if(curDate >= 1 && curDate < 12){
-  greeting = "Good Morning";
-  cssStyle.color = "orange"
-} else if (curDate >=12 && curDate < 19) {
-  greeting = "Good Afternoon";
-  cssStyle.color = "green"
-
-} else {
-  greeting = "Good Night"
-  cssStyle.color = "maroon"
-
-}
-
-ReactDOM.render(<h1> Hello Sir, <span style = {cssStyle}>{greeting}</span></h1>,document.getElementById("root"))
+import  ReactDOM from "react-dom"
+import Header from "./Header"
+ReactDOM.render(
+  <>
+  <Header />
+<p> My name is Aarju Pariyar</p>
+<p> List of hobbies are as follows</p>
+<ol>
+  <li>Cooking</li>
+  <li>playing</li>
+  <li>Dancing</li>
+  <li>Visiting</li>
+  <li>Singing</li>
+</ol>
+</>,
+document.getElementById("root")
+)
