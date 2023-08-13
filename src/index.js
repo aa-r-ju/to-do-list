6c932e4b577cad96fddc938dd004f6977bef2d35
aa-r-ju <aarju3447@gmail.com>
2023-08-13 17:02:38 +0545

    completed mini project

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from "react"
 import  ReactDOM from "react-dom"
 import Header from "./Header"
+import Para from "./Para"
+import List from "./List"
 ReactDOM.render(
   <>
   <Header />
-<p> My name is Aarju Pariyar</p>
-<p> List of hobbies are as follows</p>
-<ol>
-  <li>Cooking</li>
-  <li>playing</li>
-  <li>Dancing</li>
-  <li>Visiting</li>
-  <li>Singing</li>
-</ol>
+  <Para/>
+  <List/>
 </>,
 document.getElementById("root")
 )
