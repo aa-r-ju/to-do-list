7391ddbc836466f26e9051b71c92d73db8fbf8ff
aa-r-ju <aarju3447@gmail.com>
2023-08-13 22:44:11 +0545

    completed challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import React from "react"
-import Header from "./Header"
-import Para from "./Para"
-import List from "./List"
+import React from "react";
+
 
 function App() {
-    return (
+    let curDate = new Date(2023,8,25,18);
+curDate = curDate.getHours();
+ let greeting = " ";
+let cssStyle = { };
+    if (curDate >= 1 && curDate < 12) {
+        greeting = "Good Morning"
+        cssStyle.color = "orange"
+    } else if (curDate >= 12 && curDate < 17) {
+        greeting = "Good Afternoon"
+        cssStyle.color = "green"
+
+    } else if(curDate >= 17 && curDate < 20){
+        greeting = "Good Evening"
+        cssStyle.color = "Maroon"
+    }
+    else {
+        greeting = "Good Night"
+        cssStyle.color = "red"
+}
+   return (
     <>
-        <Header/>
-        <Para/>
-        <List/>
+        <h1> Hello sir,<span style = {cssStyle}> {greeting} </span> </h1>
     </>
-    )
+);   
 }
 export default App;
