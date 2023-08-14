5cabff3dd534886290eb9ca600d7161e258b18ba
aa-r-ju <aarju3447@gmail.com>
2023-08-14 09:27:07 +0545

    feat:complete mini projects

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import  ReactDOM from "react-dom"
 import {add,mul,min,divi} from "./App"
 ReactDOM.render(
     <>
-<p>Sum of two numbers is{add(10,20)}</p>
-<p>Multiply of two numbers is{mul(10,20)}</p>
-<p>Minus of two numbers is{min(10,20)}</p>
-<p>Divide of two numbers is{divi(10,3)}</p>
+<p>Sum of two numbers is {add(10,20)}</p>
+<p>Multiply of two numbers is {mul(10,20)}</p>
+<p>Minus of two numbers is {min(10,20)}</p>
+<p>Divide of two numbers is {divi(10,3)}</p>
 </>,
 document.getElementById("root"))
